07b30e66b6318ee13a3c961e26309bd923600a56
windlace <44790541+windlace@users.noreply.github.com>
2020-05-08 07:20:05 +0200

    Update Secp256k1Test.php

--- tests/Secp256k1Test.php
+++ tests/Secp256k1Test.php
@@ -11,6 +11,8 @@ use function Cast\Crypto\ECDSA\secp256k1\publicKeyVerbose;
 use function Cast\Crypto\ECDSA\secp256k1\publicKeyCoords;
 use function Cast\Crypto\ECDSA\secp256k1\decompressPublicKey;
 use function Cast\Crypto\ECDSA\secp256k1\hex;
+use function Cast\Crypto\ECDSA\secp256k1\sign;
+use function Cast\Crypto\ECDSA\secp256k1\verify;
 use PHPUnit\Framework\TestCase;
 
 class Secp256k1Test extends TestCase
@@ -91,5 +93,26 @@ class Secp256k1Test extends TestCase
     public function test_decompressPublicKey()
     {
         $this->assertEquals('0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6', decompressPublicKey('0250863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B2352'));
+    }
+
+    public function test_sign()
+    {
+        $privateKey = '18e14a7b6a307f426a94f8114701e7c8e774e7f9a47e2c2035db29a206321725';
+        $message = "public data to sign";
+        $messageHash = hash('sha256', $message); // hex, any algo
+        $salt = '830769f9b99527c7ecc30ed760c33a93e61041de0707338f73b4cb05823ea052'; // the sign request id, onetime random number
+
+        $this->assertEquals('be491708002b9ea80b4f09546f94c39b8865b94f2d0593eead293ef1ecc2f4bb', sign($salt, $messageHash, $privateKey));
+    }
+
+    public function test_verify()
+    {
+        $publicKey = '0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352';
+        $message = "public data to sign";
+        $messageHash = hash('sha256', $message); // hex, any algo
+        $salt = '830769f9b99527c7ecc30ed760c33a93e61041de0707338f73b4cb05823ea052'; // the sign request id, onetime random number
+        $signature = 'be491708002b9ea80b4f09546f94c39b8865b94f2d0593eead293ef1ecc2f4bb';
+
+        $this->assertEquals(true, verify($signature, $messageHash, $salt, $publicKey));
     }
 }
