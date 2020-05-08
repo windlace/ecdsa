7b5e86670b0b93be1272a873fe432cd0be97806e
windlace <44790541+windlace@users.noreply.github.com>
2020-05-08 05:52:54 +0200

    Update ecc_functions.php

--- src/ecc_functions.php
+++ src/ecc_functions.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Cast\Crypto\ECDSA\ECC;
 
-use const Cast\BaseConv\BASE_16_UPPER;
-use const Cast\BaseConv\BASE_2;
+use const Cast\Crypto\ECDSA\Conv\BASE_2;
+use const Cast\Crypto\ECDSA\Conv\BASE_16_UPPER;
 use function Cast\Crypto\ECDSA\Conv\convBase;
 
 /**
